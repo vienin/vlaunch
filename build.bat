bb0f685f834d10c06bb61219b913ecff3d5ed4d1
bob <bob@5f27ed79-0932-0410-95b1-b6313193bf37>
2009-07-13 14:56:27 +0000

    Do not include manifest into ufo.exe

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ copy ufo-generic.gif dist\.VirtualBox
 copy ufo-generic.bmp dist\.VirtualBox
 copy updater-install.gif dist\.VirtualBox
 copy updater-download.gif dist\.VirtualBox
-mt -inputresource:dist\ufo.exe;#1 -manifest ufo.exe.manifest -outputresource:dist\ufo.exe;#1
+; mt -inputresource:dist\ufo.exe;#1 -manifest ufo.exe.manifest -outputresource:dist\ufo.exe;#1
 cd dist
 ; rename launcher.exe ufo.exe
 mkdir bin
@@ -21,6 +21,6 @@ mkdir bin\drivers\network
 mkdir bin\drivers\network\netflt
 move /Y bin\VBoxDrv.sys bin\drivers\VBoxDrv
 copy /Y ..\snetcfg_x86.exe bin
-C:\Python25\python.exe -c  "import glob, tarfile; tar = tarfile.open('windows.tgz', 'w:gz'); map(tar.add, glob.glob('*.dll')); tar.add('ufo.exe'); tar.add('tcl'); tar.add('bin'); tar.close();"
+C:\Python25\python.exe -c  "import glob, tarfile; tar = tarfile.open('windows.tgz', 'w:gz'); map(tar.add, glob.glob('*.dll')); tar.add('ufo.exe'); tar.add('ufo.exe.manifest'); tar.add('tcl'); tar.add('bin'); tar.close();"
 cd ..
 pscp.exe dist\windows.tgz bob@kickstart.agorabox.org:/var/www/html/private/virtualization/windows.tgz
