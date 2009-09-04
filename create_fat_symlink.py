98292ffc252961b86b6b5948580a4cd3dff59deb
bob <bob@5f27ed79-0932-0410-95b1-b6313193bf37>
2009-09-04 14:27:12 +0000

    Forget create_fat_symlink.py

---
+++ create_fat_symlink.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os
+import sys
+import md5
+
+filename = sys.argv[2]
+target = sys.argv[1] # os.readlink(filename)
+length = "%04d" % len(target)
+buf = "XSYM\n%s\n%s\n%s\n%s" % (length, md5.md5(target).hexdigest(), target, " " * (1023 - int(length)))
+if os.path.exists(filename):
+    os.unlink(filename)
+print buf
+open(filename, "w").write(buf)
