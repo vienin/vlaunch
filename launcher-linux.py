758f98ac992e1817c868e2c6417e02b023f89e95
bob <bob@5f27ed79-0932-0410-95b1-b6313193bf37>
2009-09-04 14:50:07 +0000

    Insert 'bin' folder at the beginning of PATH

--- launcher-linux.py
+++ launcher-linux.py
@@ -4,5 +4,5 @@ import sys
 from os.path import realpath, join, dirname
 
 bindir = join(realpath(dirname(sys.argv[0])), "bin")
-sys.path.append(bindir)         
+sys.path.insert(0, bindir)         
 execfile(join(bindir, "launcher.py"))
