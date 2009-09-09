868deba8ac7f8124ef31c72ad672c67d55dbad11
marc <marc@5f27ed79-0932-0410-95b1-b6313193bf37>
2009-09-09 17:53:01 +0000

    Version fonctionnant sur qt4.3 et qt4.4

--- gui_pyqt.py
+++ gui_pyqt.py
@@ -1,9 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from PyQt4 import QtGui, QtCore, QtNetwork
-import sys, os
+from PyQt4 import QtGui, QtCore
+import threading,sys,os
+from urllib import urlretrieve
 
-app = QtGui.QApplication(sys.argv)
+
+class MyNoneEvent(QtCore.QEvent):
+    def __init__(self, size,total):
+        super(MyNoneEvent,self).__init__(QtCore.QEvent.None)
+        self.size = size
+        self.total = total
+    pass
+
+class MyFinishedEvent(QtCore.QEvent):
+    def __init__(self,error=False):
+        super(MyFinishedEvent,self).__init__(QtCore.QEvent.None)
+        self.error = error
+    pass
+
+class MyApp(QtGui.QApplication):
+    def __init__(self, argv):
+        self.progressDialog = None
+        self.DownloadWindow = None
+        QtGui.QApplication.__init__(self, argv)
+
+    def event(self,event):
+        if isinstance(event,MyFinishedEvent):
+            self.DownloadWindow.httpRequestFinished(event.error)
+        elif self.progressDialog and isinstance(event,MyNoneEvent):
+           self.progressDialog.setMaximum(event.total)
+           self.progressDialog.setValue(event.size)
+        return False
+
+app = MyApp(sys.argv)
 desktop = app.desktop()
 screenRect = desktop.screenGeometry(desktop.primaryScreen())
 main = QtGui.QMainWindow(desktop)
@@ -22,6 +51,40 @@ def create_app():
 def destroy_app(app):
     app.exit()
     app = None
+
+
+# Gere le téléchargement dans un thread a part.
+# Envoie Deux type d'evenement à l'application :
+#   1. MyNoneEvent pour chaque mise a jour de la progression du téléchargemetn
+#   2. MyFinishedEvent quand il termine(sur une erreur ou non)
+# ATTENTION: pour chaque appel de stop par le thread principale il faut recréer l'objet downloader
+class Downloader(threading.Thread):
+        def __init__(self,file,dest): 
+                threading.Thread.__init__(self)
+                self.file=file
+                self.dest=dest
+                self.toBeStop=False
+
+        def run(self):
+                try:
+                    yeah, headers = urlretrieve(self.file, self.dest, reporthook=self.progress)
+                except :
+                    app.postEvent(app,MyFinishedEvent(True))
+                else: 
+                    app.postEvent(app,MyFinishedEvent(False))
+                sys.exit()
+
+        def progress(self,count,blockSize,totalSize):
+                if self.toBeStop==True:
+                    sys.exit()
+                self.count=count
+                self.maximum=totalSize
+                self.downloaded=blockSize*count*100/totalSize
+                app.postEvent(app, MyNoneEvent(int(self.downloaded),100))
+
+        def stop(self):
+                self.toBeStop=True
+
 
 class OurMessageBox(QtGui.QMessageBox):
     def setMinimumSize(self, width, height):
@@ -63,22 +126,25 @@ def dialog_question(title, msg, button1="Yes", button2="No"):
     else: return button2
 
 def dialog_password(msg=None, rcode=False):
-    dlg = QtGui.QInputDialog(main)
-    dlg.setTextEchoMode(QtGui.QLineEdit.Password)
+    w=QtGui.QWidget()
+    #dlg = QtGui.QInputDialog(main)
+    #dlg.setTextEchoMode(QtGui.QLineEdit.Password)
     if not msg:
-        msg = u"Veuillez entrer le mot de passe de l'utilisateur \"" + os.environ["USER"] + "\""
-    dlg.setLabelText(msg)
-    dlg.setWindowTitle(u"Autorisations nécessaires")
-    ret = dlg.exec_()
+        msg = u"Veuillez entrer le mot de passe de l'utilisateur " + os.environ["USER"]
+    #dlg.setLabelText(msg)
+    #dlg.setWindowTitle(u"Autorisations nécessaires")
+    #dlg.exec_()
+    value = QtGui.QInputDialog.getText(w,"Saisi de mot de passe",msg,QtGui.QLineEdit.Password)
     if rcode:
-        return dlg.textValue(), ret
+        return value[0], ret
     else:
-        return dlg.textValue()
+        return value[0]
 
 class DownloadWindow(QtGui.QDialog):
     def __init__(self, url, filename, parent=None):
         super(DownloadWindow, self).__init__(parent)
 
+        app.DownloadWindow = self
         self.url = url
         self.fileName = filename
         self.outFile = None
@@ -86,16 +152,18 @@ class DownloadWindow(QtGui.QDialog):
         self.httpRequestAborted = False
         self.finished = False
 
-        self.statusLabel = QtGui.QLabel(self.tr(u"Un live U.F.O est nécessaire pour continuer. \n"   
+        self.statusLabel = QtGui.QLabel(u"Un live U.F.O est nécessaire pour continuer. \n"   
                                                 u"Cliquez sur 'Télécharger' pour commencer le téléchargement.\n\n"
                                                 u"Cette opération peut prendre de quelques minutes à plusieurs heures\n" 
-                                                u"suivant la vitesse de votre connexion."))
+                                                u"suivant la vitesse de votre connexion.")
 
         self.progressDialog = QtGui.QProgressDialog(self)
+        app.progressDialog = self.progressDialog
 
-        self.downloadButton = QtGui.QPushButton(self.tr(u"Télécharger"))
+        self.progressDialog.setCancelButtonText(u"Annuler")
+        self.downloadButton = QtGui.QPushButton(u"Télécharger")
         self.downloadButton.setDefault(True)
-        self.quitButton = QtGui.QPushButton(self.tr(u"Quitter"))
+        self.quitButton = QtGui.QPushButton(u"Quitter")
         self.quitButton.setAutoDefault(False)
 
         buttonBox = QtGui.QDialogButtonBox()
@@ -103,15 +171,14 @@ class DownloadWindow(QtGui.QDialog):
                 QtGui.QDialogButtonBox.ActionRole)
         buttonBox.addButton(self.quitButton, QtGui.QDialogButtonBox.RejectRole)
 
-        self.http = QtNetwork.QHttp(self)
 
-        self.http.requestFinished.connect(self.httpRequestFinished)
-        self.http.dataReadProgress.connect(self.updateDataReadProgress)
-        self.http.responseHeaderReceived.connect(self.readResponseHeader)
+        # On prépare le thread qui gérera le téléchargement
+        self.initDownloader()
 
-        self.progressDialog.canceled.connect(self.cancelDownload)
-        self.downloadButton.clicked.connect(self.downloadFile)   
-        self.quitButton.clicked.connect(self.close)
+        # On se connecte au Slot Qt pointant sur les evenement C++, sender,SIGNAL,callable
+        self.connect(self.progressDialog,QtCore.SIGNAL("canceled()"),self.cancelDownload)
+        self.connect(self.downloadButton,QtCore.SIGNAL("clicked()"),self.downloadFile)
+        self.connect(self.quitButton,QtCore.SIGNAL("clicked()"),self.close)
 
         topLayout = QtGui.QHBoxLayout()
 
@@ -121,7 +188,11 @@ class DownloadWindow(QtGui.QDialog):
         mainLayout.addWidget(buttonBox)
         self.setLayout(mainLayout)
 
-        self.setWindowTitle(self.tr(u"Téléchargement de l'image"))
+        self.setWindowTitle(u"Téléchargement de l'image")
+
+    def initDownloader(self):
+        self.downloader=Downloader(self.url,self.fileName)
+        
 
     def downloadFile(self):
         if self.finished:
@@ -130,31 +201,32 @@ class DownloadWindow(QtGui.QDialog):
         fileName = self.fileName
         self.outFile = QtCore.QFile(fileName)
         if not self.outFile.open(QtCore.QIODevice.WriteOnly):
-            QtGui.QMessageBox.information(self, self.tr("HTTP"),
-                    self.tr(u"Impossible d'écrire dans le fichier %1: %2.").arg(fileName).arg(self.outFile.errorString()))
+            QtGui.QMessageBox.information(self, "HTTP",
+                    "Impossible d'écrire dans le fichier %1: %2.".arg(fileName).arg(self.outFile.errorString()))
             self.outFile = None
             return
 
-        self.http.setHost(QtCore.QUrl(self.url).host())
-
-        # request = self.http.request(self.url)
-        self.httpGetId = self.http.get(self.url, self.outFile)
-
-        self.progressDialog.setWindowTitle(self.tr(u"Téléchargement de l'image"))
-        self.progressDialog.setLabelText(self.tr(u"Téléchargement en cours : %1.").arg(fileName))
+        self.progressDialog.setWindowTitle(u"Téléchargement de l'image")
+        self.progressDialog.setLabelText(u"Téléchargement en cours : %s"%(fileName,))
+        self.progressDialog.show()
+        
         self.downloadButton.setEnabled(False)
+        self.downloader.start()
+
 
     def cancelDownload(self):
-        self.statusLabel.setText(self.tr(u"Téléchargement annulé. \n"
-                                         u"Cliquer sur 'Télécharger' pour recommencer."))
+        self.statusLabel.setText(u"Téléchargement annulé. \n"
+                                         u"Cliquer sur 'Télécharger' pour recommencer.")
         self.httpRequestAborted = True
-        self.http.abort()
+        self.downloader.stop()
+        self.initDownloader()
         self.downloadButton.setEnabled(True)
 
-    def httpRequestFinished(self, requestId, error):
-        if requestId != self.httpGetId:
-            return
+    def httpRequestFinished(self,  error):
+        if self.downloader.isAlive():
+            self.downloader.join()
 
+        self.initDownloader()
         if self.httpRequestAborted:
             if self.outFile is not None:
                 self.outFile.close() 
@@ -169,35 +241,20 @@ class DownloadWindow(QtGui.QDialog):
 
         if error:
             self.outFile.remove()
-            QtGui.QMessageBox.information(self, self.tr("HTTP"),
-                    self.tr(u"Le téléchargement a échoué. Vérifiez votre connexion Internet."))
+            QtGui.QMessageBox.information(self, "HTTP",
+                    u"Le téléchargement a échoué. Vérifiez votre connexion Internet.")
         else:
             self.finished = True
-            self.statusLabel.setText(self.tr(u"Téléchargement terminé. Cliquez sur 'Démarrer' pour continuer."))
-            self.downloadButton.setText(u"Démarrer")
+            # On cache les boutons
+            self.downloadButton.hide()
+            self.quitButton.setText(u"Continuer")
+            self.statusLabel.setText(u"Téléchargement terminé.")
 
         self.downloadButton.setEnabled(True)
         self.outFile = None
 
-    def readResponseHeader(self, responseHeader):
-        # Check for genuine error conditions.
-        if responseHeader.statusCode() not in (200, 300, 301, 302, 303, 307):
-            QtGui.QMessageBox.information(self, self.tr("HTTP"),
-                                          self.tr("Le téléchargement est actuellement indisponible. " \
-                                                  "Merci de bien vouloir réesayer plus tard.")) # responseHeader.reasonPhrase()))
-            self.httpRequestAborted = True
-            self.progressDialog.hide()
-            self.http.abort()
-
-    def updateDataReadProgress(self, bytesRead, totalBytes):
-        if self.httpRequestAborted:
-            return
-
-        self.progressDialog.setMaximum(totalBytes)
-        self.progressDialog.setValue(bytesRead)   
-
 def download_file(url, filename):
-    downloadWin = DownloadWindow(url=url, filename=filename) # , parent=main)
+    downloadWin = DownloadWindow(url=url, filename=filename)
     downloadWin.show()
     return downloadWin.exec_()
 
