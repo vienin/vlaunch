6939cdd9fc98962e1bcd6fec3e6ac15e04c54a6a
bob <bob@5f27ed79-0932-0410-95b1-b6313193bf37>
2009-09-09 12:46:07 +0000

    Insert manifest into ufo.exe (to run as root on Vista)

--- setup.py
+++ setup.py
@@ -4,10 +4,23 @@ from py2exe.build_exe import py2exe as BuildExe
 import os, sys
 import glob
 
+manifest = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0"> 
+   <assemblyIdentity version="1.0.0.0" processorArchitecture="X86" name="ufo" type="win32"/>
+      <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+      <security>
+         <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator"/> 
+         </requestedPrivileges>
+      </security>
+   </trustInfo>
+</assembly>
+"""
+
 setup(zipfile = None,
       options = {'py2exe': { 'bundle_files': 1,
                               'includes': ['sip'],
-                              #'excludes' : [ "Tkconstants", "Tkinter", "tcl",
+                              'excludes' : [ "Tkconstants", "Tkinter", "tcl" ],
                               #               "PyQt4.QtCore", "PyQt4.QtGui", "PyQt4.QtNetwork", "PyQt4" ],
                               # 'dll_excludes': [ "PyQt4\\QtCore.pyd", "PyQt4\\QtGui.pyd",
                               #                   "PyQt4\\QtNetwork.pyd",
@@ -15,5 +28,8 @@ setup(zipfile = None,
                               "typelibs": [('{46137EEC-703B-4FE5-AFD4-7C9BBBBA0259}', 0, 1, 3)],
                 }},
 
-      windows = [{'script': "ufo.py", "icon_resources" : [(1, "UFO.ico")]}],
+      windows = [{'script': "ufo.py",
+                   "icon_resources" : [(1, "UFO.ico")],
+                   "other_resources": [(24, 1, manifest)],
+                  }],
 )
