1d0e9c34bc8924bfc6fa98834d6d109ab6ca5d02
bob <bob@5f27ed79-0932-0410-95b1-b6313193bf37>
2009-11-03 12:28:01 +0000

    vlaunch: fix setup-mac and Makefile.mac

---
+++ build/setup.py
@@ -0,0 +1,54 @@
+# UFO-launcher - A multi-platform virtual machine launcher for the UFO OS
+#
+# Copyright (c) 2008-2009 Agorabox, Inc.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. 
+
+
+from distutils.core import setup
+import py2exe
+from py2exe.build_exe import py2exe as BuildExe
+import os, sys
+import glob
+
+manifest = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0"> 
+   <assemblyIdentity version="1.0.0.0" processorArchitecture="X86" name="ufo" type="win32"/>
+      <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+      <security>
+         <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator"/> 
+         </requestedPrivileges>
+      </security>
+   </trustInfo>
+</assembly>
+"""
+
+setup(zipfile = None,
+      options = {'py2exe': { 'bundle_files': 1,
+                              'includes': ['sip', 'win32com.server.util', 'pythoncom'],
+                              'excludes' : [ "Tkconstants", "Tkinter", "tcl" ],
+                              #               "PyQt4.QtCore", "PyQt4.QtGui", "PyQt4.QtNetwork", "PyQt4" ],
+                              # 'dll_excludes': [ "PyQt4\\QtCore.pyd", "PyQt4\\QtGui.pyd",
+                              #                   "PyQt4\\QtNetwork.pyd",
+                              #                   "QtCore4.dll", "QtGui4.dll", "QtNetwork4.dll" ],
+                              "typelibs": [('{46137EEC-703B-4FE5-AFD4-7C9BBBBA0259}', 0, 1, 3)],
+                }},
+
+      windows = [{'script': "ufo.py",
+                   "icon_resources" : [(1, "../graphics/UFO.ico")],
+                   "other_resources": [(24, 1, manifest)],
+                  }],
+)
