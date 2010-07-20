6045521897688378457e00fe3976c2983662f189
Sylvain Baubeau <sylvain.baubeau@agorabox.org>
2010-07-20 14:59:30 +0200

    New partitionning method

--- src/mbr.py
+++ src/mbr.py
@@ -47,9 +47,7 @@ class MBR:
         cyl_units = h * s * BLOCK_SIZE
         totalsize = cyl_units
         offset = 0
-        lba = 1
-        start_head = 0
-        start_sector = 2
+        lba = s
         self.partitions = []
         for n, part in enumerate(partitions):
             type, size, bootable = part["type"], part["size"], part["bootable"]
@@ -59,6 +57,7 @@ class MBR:
                 cylinders = size / cyl_units
                 if size % cyl_units:
                     cylinders += 1
+            cylinders = min(cylinders, c - offset)
             sectors = cylinders * cyl_units / BLOCK_SIZE
             if bootable:
                 status = 0x80
@@ -67,31 +66,30 @@ class MBR:
             if n == 0:
                 part = {
                      "status" : status,
-                     "start_head" : start_head,
-                     "start_sector" : start_sector,
-                     "start_cylinder" : offset,
+                     "start_head" : 1,
+                     "start_sector" : 1,
+                     "start_cylinder" : 0,
                      "end_head" : h - 1,
                      "end_sector" : s,
                      "end_cylinder" : offset + cylinders - 1,
                      "type" : type,
-                     "lba" : lba,
+                     "lba" : offset * h * s + s,
                      "sectors" : sectors - lba,
                 }
                 lba += sectors - lba
             else:
                 part = {
                      "status" : status,
-                     "start_head" : start_head,
+                     "start_head" : 0,
                      "start_sector" : 1,
                      "start_cylinder" : offset,
                      "end_head" : h - 1,
                      "end_sector" : s,
                      "end_cylinder" : offset + cylinders - 1, 
                      "type" : type,
-                     "lba" : lba,
+                     "lba" : offset * h * s,
                      "sectors" : sectors,
                 }
-                lba += sectors
             offset += cylinders
             self.partitions.append(part)
     
